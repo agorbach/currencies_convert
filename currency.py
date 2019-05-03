3a5ca02394a6d11ba8fc7bb6c26ae0a1a3801970
Ivan <45122314+jeyldii@users.noreply.github.com>
2019-05-03 15:36:17 +0300

    Update currency.py

--- currency.py
+++ currency.py
@@ -7,7 +7,7 @@ from decimal import Decimal
 def convert(amount, cur_from, cur_to, date, requests):
     response = requests.get(
             'http://www.cbr.ru/scripts/XML_daily.asp?date_req={}'.format(date)
-        )  # Использовать переданный requests
+        )  
 
     soup = BeautifulSoup(response.content, 'xml')
     if cur_from == 'RUR':
@@ -41,4 +41,4 @@ def convert(amount, cur_from, cur_to, date, requests):
     else:
         result = (amount * amount_from) / amount_to
 
-    return result.quantize(Decimal('1.0000'))  # не забыть про округление до 4х знаков после запятой
+    return result.quantize(Decimal('1.0000'))  
